d71748f06dfbd9228c92e382837e555241f492ca
DavidColatti <davidcolatti@gmail.com>
2020-04-21 16:38:11 -0400

    iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,7 +6,8 @@ import contacts from './contacts.json';
 class App extends Component {
 
   state = {
-    actors: contacts.slice(0,5)
+    actors: contacts.splice(0, 5),
+    restOfActors: contacts
   }
 
   getInfo() {
@@ -22,21 +23,44 @@ class App extends Component {
         </tr>
       )
     }
-    
     return infoArray
   }
+
+  addRandomContact = () => {
+    let randomNum = Math.floor(Math.random() * this.state.restOfActors.length)
+    let randomPick = this.state.restOfActors[randomNum]
+    
+    let remainingCopy = [...this.state.restOfActors]
+    remainingCopy.splice(randomNum, 1)
+    console.log(remainingCopy)
+    let actorsCopy = [...this.state.actors]
+    actorsCopy.push(randomPick)
+
+    this.setState({
+      actors: actorsCopy,
+      restOfActors: remainingCopy
+    })
+  }
+
 
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
+        <button onClick={this.addRandomContact}>Add Random Contact</button>
+        <button>Sort By Name</button>
+        <button>Sort By Popularity</button>
         <table>
+          <thead>
             <tr>
               <th>Picture</th>
               <th>Name</th>
               <th>Popularity</th>
             </tr>
+          </thead>
+          <tbody>
           {this.getInfo()}
+          </tbody>
         </table>
       </div>
     );
