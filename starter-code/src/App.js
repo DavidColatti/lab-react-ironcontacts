a6c7ec41777f79ea044104161ede5b9063d79c55
DavidColatti <davidcolatti@gmail.com>
2020-04-21 13:19:42 -0400

    iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+
 
 class App extends Component {
+
+  state = {
+    actors: contacts.slice(0,5)
+  }
+
+  getInfo() {
+    let actors = this.state.actors
+    let infoArray = []
+
+    for (let i = 0; i < actors.length; i++) {
+      infoArray.push(
+        <tr>
+          <td><img src={actors[i].pictureUrl} class="photo"/></td>
+          <td>{actors[i].name}</td>
+          <td>{actors[i].popularity}</td>
+        </tr>
+      )
+    }
+    
+    return infoArray
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>IronContacts</h1>
+        <table>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          {this.getInfo()}
+        </table>
       </div>
     );
   }
